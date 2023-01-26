f0ae290a1fae64aa48b8747f32f646e68ca835b3
Upoma21801~ <upoma.cdsr@gmail.com>
2023-01-27 01:13:58 +0600

    all api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,67 +10,66 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4cizlao.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4cizlao.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// async function run() {
-//     try {
-//         const patientDetailsCollection = client.db('patientCare').collection('patientDetails');
+async function run() {
+    try {
+        const itemsCollection = client.db('InvestoryManagement').collection('items');
 
-//         // Patient get api
-//         app.get('/details', async (req, res) => {
-//             const query = {};
-//             const detail = await patientDetailsCollection.find(query).toArray();
-//             res.send(detail);
-//         })
-//         app.get('/details/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const filter = { _id: ObjectId(id) };
-//             const result = await patientDetailsCollection.findOne(filter);
-//             res.send(result);
-//         })
-        
-//         app.post('/details', async (req, res) => {
-//             const detail = req.body;
-//             const result = await patientDetailsCollection.insertOne(detail);
-//             res.send(result);
+        // Items get api
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const item = await itemsCollection.find(query).toArray();
+            res.send(item);
+        })
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await itemsCollection.findOne(filter);
+            res.send(result);
+        })
 
-//         });
+        app.post('/items', async (req, res) => {
+            const item = req.body;
+            const result = await itemsCollection.insertOne(item);
+            res.send(result);
 
-//         // // Edit Patient api
-//         app.put('/editInfo/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const info = req.body;
-//             console.log(info)
-//             const filter = { _id: ObjectId(id) };
-//             const options = { upsert: true };
-//             const updatedDoc = {
-//                 $set : {
-//                     email: info.email,
-//                     phone: info.phone,
-//                     address: info.address,
-//                     disease: info.disease,
-//                     condition: info.condition,
-//                 }
-//             }
-//             const result = await patientDetailsCollection.updateOne(filter, updatedDoc, options);
-//             res.send(result)
-//         })
+        });
 
-//         // // delete patient
-//         app.delete('/details/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: ObjectId(id) };
-//             const result = await patientDetailsCollection.deleteOne(query);
-//             // console.log(result);
-//             res.send(result);
-//         })
-//     }
-//     finally {
+        // Edit item api
+        app.put('/editInfo/:id', async (req, res) => {
+            const id = req.params.id;
+            const info = req.body;
+            console.log(info)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: info.name,
+                    date: info.date,
+                    owner: info.owner,
+                    vendor: info.vendor
+                }
+            }
+            const result = await itemsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
 
-//     }
-// }
-// run().catch(err => console.error(err));
+        // delete item
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            // console.log(result);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Inventory management app is running now on port 5000');
